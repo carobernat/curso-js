bf98eda12fba0ef236d2fcaf8deba299a08feca1
Caro <carolina.bernat@iunigo.com>
2019-03-09 16:16:39 -0300

    ejercicio switch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+/*
+Calculadora
+
 var num1 = parseInt(prompt("Primer número"));
 var sign = prompt("Operación");
 var num2 = parseInt(prompt("Segundo número?"));
@@ -17,5 +20,27 @@ switch(sign) {
         break;
     default:
         alert("No pusiste ningún signo válido!")
+}
+*/
+
+var day = prompt("Qué día querés chequear?").toUpperCase()
+
+switch(day) {
+    case "LUNES":
+    case "MARTES":
+    case "MIERCOLES":
+    case "JUEVES":
+    case "VIERNES":
+        alert("El día ingresado es hábil");
+        break;
+    case "SABADO":
+    case "DOMINGO":
+        alert("El día ingresado no es hábil");
+        break;
+    default:
+        alert("El dato ingresado no es válido");
+        break;
+
+
 }
 
