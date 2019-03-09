12d0f839c68de48900cd4afeb27f10f3cb61804b
Caro <carolina.bernat@iunigo.com>
2019-03-09 15:07:19 -0300

    Primer iteración maquetado

---
+++ js/index.js
@@ -0,0 +1,12 @@
+var name = prompt("Cuál es tu nombre?");
+var num = prompt("Qué número querés sumar?");
+var num2 = 2;
+var num3 = prompt("Qué número querés restar?");
+var num4 = prompt("Qué número querés sumar?");
+var num5 = prompt("Con qué otro número?");
+
+console.log("Hola " + name);
+console.log(parseInt(num) + num2);
+console.log(parseInt(num3) - num2);
+console.log(parseInt(num4) + parseInt(num5));
+
