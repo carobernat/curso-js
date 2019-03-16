1056b08329578f9e11156d654502963810af411a
Caro <carolina.bernat@iunigo.com>
2019-03-16 12:18:16 -0300

    ejercicios en un solo archivo

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,3 +1,64 @@
+/*
+Ejercicios:
+
+var name = prompt("Cuál es tu nombre?");
+var num = prompt("Qué número querés sumar?");
+var num2 = 2;
+var num3 = prompt("Qué número querés restar?");
+var num4 = prompt("Qué número querés sumar?");
+var num5 = prompt("Con qué otro número?");
+
+console.log("Hola " + name);
+console.log(parseInt(num) + num2);
+console.log(parseInt(num3) - num2);
+console.log(parseInt(num4) + parseInt(num5));
+
+
+Calculadora
+
+var num1 = parseInt(prompt("Primer número"));
+var sign = prompt("Operación");
+var num2 = parseInt(prompt("Segundo número?"));
+
+switch(sign) {
+    case "+":
+        alert(num1 + num2);
+        break;
+    case "-":
+        alert(num1 - num2);
+        break;
+    case "*":
+        alert(num1 * num2);
+        break;
+    case "/":
+        alert(num1 / num2);
+        break;
+    default:
+        alert("No pusiste ningún signo válido!")
+}
+
+Ejercicio switch:
+
+var day = prompt("Qué día querés chequear?").toUpperCase()
+
+switch(day) {
+    case "LUNES":
+    case "MARTES":
+    case "MIERCOLES":
+    case "JUEVES":
+    case "VIERNES":
+        alert("El día ingresado es hábil");
+        break;
+    case "SABADO":
+    case "DOMINGO":
+        alert("El día ingresado no es hábil");
+        break;
+    default:
+        alert("El dato ingresado no es válido");
+        break;
+}
+
+
 var gender = prompt("Género: Sos hombre, mujer o indefinido?").toLowerCase();
 var age = parseInt(prompt("Cuántos años tenés?"));
 
@@ -16,3 +77,9 @@ if (age < 18 && gender === "mujer") {
 } else {
     alert("Todos los datos son necesarios")
 }
+
+*/
+
+
+
+
