ef8a567401fdd8aa0fe1bbfe12c812570e7da221
Caro <carolina.bernat@iunigo.com>
2019-04-12 19:16:11 -0300

    workshop

---
+++ desafios/desafio_8.js
@@ -0,0 +1,28 @@
+
+var email = document.getElementById('email');
+var emailHelp = document.getElementById('emailHelp');
+
+email.onblur = verifyEmail;
+email.onclick = resetStatus;
+
+function verifyEmail(event){
+    var emailNode =  event.target;
+    if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(emailNode.value))
+    {
+        email.classList.add("is-valid");
+        emailHelp.innerText = "El mail ingresado es válido.";
+
+    } else {
+        email.classList.add("is-invalid");
+        emailHelp.innerText = "El mail ingresado es inválido.";
+    }
+}
+
+function resetStatus(event) {
+    var emailNode = event.target;
+    emailNode.value.clear;
+    emailHelp.innerText = "";
+    email.classList.remove("is-invalid", "is-valid");
+}
+
+
